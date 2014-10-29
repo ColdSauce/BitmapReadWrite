d793d2902555e786f9e2b513d0c9f5ece9671ad2
coldsauce <stefan@stefanaleksic.com>
2014-10-28 20:32:00 -0700

    working on bitmap structs

--- main.c
+++ main.c
@@ -4,8 +4,21 @@ int main(){
     
 }
 
+//The following code is taken from http://stackoverflow.com/questions/14279242/read-bitmap-file-into-structure. 
+//I wanted to write out the program and then look at it in an attempt to learn C a little bit better.
+
+
 #pragma pack(push, 1)
 
+typedef struct tagBITMAPFILEHEADER{
+    WORD bfType; //tells you what the file type is.
+}
+    
+
+
+#pragma pack(pop)
+
+#pragma pack(push, 1)
 typedef struct tagBITMAPINFOHEADER{
     DWORD biSize;
     LONG biWidth;
@@ -13,4 +26,26 @@ typedef struct tagBITMAPINFOHEADER{
     WORD biPlanes;
     WORD biBitCount;
     DWORD biCompression;
+    DWORD BiSizeImage;
+    LONG biXPelsPerMeter;
+    LONG biYPelsPerMeter;
+    DWORD biClrUsed;
+    DWORD BiClrImportant;
+} BITMAPINFOHEADER;
+
+
+#pragma pack(pop)
+
+unsigned char *LoadBitmapFile(char *filename, BITMAPINFOHEADER *bitmapInfoHeader){
+    FILE *filePointer;
+    BITMAPFILEHEADER bitmapFileHeader;
+    unsigned char *bitmapImage;
+    unsigned char tempRGB;
+    
+    filePtr = ropen(filename,"rb");
+    if(filePtr == NULL){
+        return NULL;
+    }
+
+    //So now the program has to read the header so that it can see if 
 }

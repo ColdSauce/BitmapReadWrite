e29d406289697404b8859b14541c525e8c9950cc
coldsauce <stefan@stefanaleksic.com>
2014-10-28 20:23:11 -0700

    added struct to represent header

--- main.c
+++ main.c
@@ -4,4 +4,13 @@ int main(){
     
 }
 
+#pragma pack(push, 1)
 
+typedef struct tagBITMAPINFOHEADER{
+    DWORD biSize;
+    LONG biWidth;
+    LONG biHeight;
+    WORD biPlanes;
+    WORD biBitCount;
+    DWORD biCompression;
+}
